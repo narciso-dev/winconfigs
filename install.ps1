c74c533d32217e62848199bd133218688a57a768
Alessandro Narciso <alessandro@narciso.dev>
2020-10-09 15:15:55 +0200

    Added choco installer

---
+++ install.ps1
@@ -0,0 +1,16 @@
+# Installing development tools
+
+# Install GIT
+choco install -y git
+
+# Install NodeJS
+choco install -y nodejs
+
+# Install NPM
+choco install -y npmtaskrunner
+
+# Install yarn
+choco install -y yarn
+
+# Install vscode
+choco install -y vscode
